bdd115dc59e503e7b1f572411a4d0a9a80656731
Rabin Rai <rabinrai44@gmail.com>
2018-06-16 16:34:21 -0400

    Added basics functionality, accept user two difference dates, print the value in days, min, sec

--- Lab102DifferenceBetweenDate/Program.cs
+++ Lab102DifferenceBetweenDate/Program.cs
@@ -6,7 +6,24 @@ namespace Lab102DifferenceBetweenDate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Define variables 
+            DateTime FirstDate;
+            DateTime LastDate;
+            TimeSpan DateDifference;
+
+            // inputs 
+            Console.WriteLine("Please enter first date in dd/mm/yyyy format");
+            FirstDate = Convert.ToDateTime(Console.ReadLine()).Date;
+
+            Console.WriteLine("Please enter last date in dd/mm/yyyy format");
+            LastDate = Convert.ToDateTime(Console.ReadLine()).Date;
+
+             LastDate.Subtract(FirstDate);
+            
+            // Print the result
+            Console.WriteLine(String.Format("{0} days, {1} hours, {2} minutes, {3} seconds", DateDifference.Days, DateDifference.Hours, DateDifference.Minutes, DateDifference.Seconds));
+            Console.ReadLine();
+
         }
     }
 }
